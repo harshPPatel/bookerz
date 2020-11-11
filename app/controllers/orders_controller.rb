4e9028f2cdbbd6d9a831d44661d8cbb2ff9b202d
Harsh Patel <harshppatel2880@gmail.com>
2020-11-10 18:33:46 -0600

    added functionality so that user can place order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,25 @@
 class OrdersController < ApplicationController
   def create
     session_cart = session[:cart]
+    address = Address.find(current_user.address_id)
+    province = Province.find(address.province_id)
+    order = Order.create(order_date:  DateTime.now,
+                         current_pst: 0.05,
+                         current_gst: province.current_gst,
+                         user_id:     current_user.id)
+    return unless order.save!
+
     @cart.each do |book|
-      
+      OrderBook.create(quantity:    session_cart[book.id.to_s].to_i,
+                       order_price: book.price,
+                       order_id:    order.id,
+                       book_id:     book.id)
     end
-    redirect_back fallback_location: root_path
+    session.delete(:cart)
+    redirect_to order_url(order)
+  end
+
+  def show
+    @id = params[:id]
   end
 end
